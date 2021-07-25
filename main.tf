bd09b251b4b73f2e95d18e75b5d23d17652be72f
syntapy <syntapy@pm.me>
2021-07-24 22:49:24 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+variable "region" { }
+variable "project_name" { }
+variable "billing_account" { }
+variable "org_id" { }
+
+locals {
+  project_prefix = join("", [lower(replace(var.project_name, " ", "-")), "-"])
+}
+
+provider "google" { 
+  region = var.region
+}
+
+data "google_billing_account" "my_billing_account" {
+  billing_account = var.billing_account
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+  prefix = local.project_prefix
+}
+
+resource "google_project" "project" {
+  name = var.project_name
+  project_id = random_id.id.hex  
+  billing_account = data.google_billing_account.my_billing_account.id
+  org_id = var.org_id
+}
+
+output "name" {
+  value = google_project.project.name
+}
+
+output "id" {
+  value = google_project.project.id
+}
+
+output "number" {
+  value = google_project.project.number
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">= 3.75"
+    }
+    random = {
+     source = "hashicorp/random"
+      version = ">= 3.1"
+    }
+  }
+}
